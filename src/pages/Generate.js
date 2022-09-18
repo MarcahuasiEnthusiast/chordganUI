c566f99fd014abc2021878777028703848c0e687
Fjorpa <38122366+Fjorpa@users.noreply.github.com>
2022-09-17 21:17:29 -0500

    Added Sliders style, buttons applied to all, fixed temperature slider

--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -2,65 +2,74 @@ import React, { Component } from 'react';
 import {Button, Typography, Divider, Item, Container, Stack, Slider, FormLabel, FormControl, InputLabel, Select, MenuItem, CircularProgress, Snackbar, Alert} from '@mui/material';
 import { Midi } from '@tonejs/midi'
 import * as Tone from 'tone'
-import './OldApp.css';
+import { styled } from '@mui/material/styles';
+import '../Generate.css';
 
-// Style sheet
-const formLabelStyling = {
-  color: "rgb(0, 0, 0)",
+const CustomSlider = styled(Slider)(({ theme }) => ({
+  color: '#272727',
   whiteSpace: "nowrap",
   textOverflow: "ellipsis",
-  overflow: "hidden"
-};
+  marginTop: "-10px !important",
+  height: 8,
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-thumb': {
+    height: 24,
+    width: 24,
+    backgroundColor: '#f2f2f2',
+    border: '2px solid currentColor',
+    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+      boxShadow: 'inherit',
+    },
+    '&:before': {
+      display: 'none',
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    lineHeight: 1.2,
+    fontSize: 10,
+    background: 'unset',
+    padding: 0,
+    width: 30,
+    height: 30,
+    borderRadius: '50% 50% 50% 0',
+    backgroundColor: '#858585',
+    transformOrigin: 'bottom left',
+    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+    '&:before': { display: 'none' },
+    '&.MuiSlider-valueLabelOpen': {
+      transform: 'translate(50%, -85%) rotate(-45deg) scale(1)',
+    },
+    '& > *': {
+      transform: 'rotate(45deg)',
+    },
+  },
+}));
 
-const sliderLabelNumberOfFilesStyling = {
-  color: "rgb(0, 0, 0)",
+const CustomFormLabel = styled(FormLabel)(({ theme }) => ({
+
+  color: '#272727',
   whiteSpace: "nowrap",
   textOverflow: "ellipsis",
-  overflow: "hidden",
-  marginTop: "-10px !important"
-}
+  overflow: 'hidden',
 
-const MIDISelectLabel = {
-  color: "rgb(0, 0, 0)",
-  whiteSpace: "nowrap",
-  textOverflow: "ellipsis",
-  overflow: "hidden",
-}
+}));
 
-const lowerMIDIPanel = {
-  marginTop: "35px",
-}
+const CustomButton = styled(Button)(({ theme }) => ({
+  color: "#fdfdfd",
+  backgroundColor: "#272727",
+  textAlign: "center",
+  borderRadius: '8px',
+  '&:hover': {
+    backgroundColor: '#858585',
+    color: '#fdfdfd',
+  },
 
-const upperPanelStyling = {
-  marginTop: "25px",
-  justifyContent: "center"
-}
 
-const lowerPanelStyling = {
-  marginTop: "20px",
-  justifyContent: "center"
-}
+}));
 
-const sliderMemoryLengthStyling = {
-  color: "rgb(0, 0, 0)",
-  marginTop: "-10px !important"
-}
-
-const generateButtonStyling = {
-  color: "rgb(255, 240, 230)",
-  backgroundColor: "rgb(0, 0, 0)"
-}
-
-const downloadButtonStyling = {
-  width: "50%",
-  height: "50%",
-  color: "rgb(255, 240, 230)",
-  backgroundColor: "rgb(0, 0, 0)",
-  top: "50px"
-}
-
-class Gemerate extends Component {
-
+class Generate extends Component {
   constructor(props) {
     super(props); // check why? study
 
@@ -68,11 +77,6 @@ class Gemerate extends Component {
       currentMidi: {},
       synths: [],
       isPlaying: false,
-
-      // Snackbar
-      openSnackbar: false,
-      verticalSnackbar: 'bottom',
-      horizontalSnackbar: 'left',
 
       generationWaitingForResponse: false,
 
@@ -82,8 +86,14 @@ class Gemerate extends Component {
       downloadDisabled: true, //true
       selectedMIDI: {},
       success: false,
-      filesArray: []
+      filesArray: [],
       // Note: think carefully before initializing state based on props!
+
+      // ESTILOS
+      // Snackbar
+      openSnackbar: false,
+      verticalSnackbar: 'bottom',
+      horizontalSnackbar: 'left',
     }
 
     this.player = React.createRef();
@@ -92,6 +102,7 @@ class Gemerate extends Component {
     this.setCurrentMIDI = this.setCurrentMIDI.bind(this)
     this.memLengthHandler = this.memLengthHandler.bind(this)
     this.numFilesHandler = this.numFilesHandler.bind(this)
+    this.temperatureHandler = this.temperatureHandler.bind(this)
     this.generate = this.generate.bind(this)
     this.selectedMIDIHandler = this.selectedMIDIHandler.bind(this)
     this.handleClick = this.handleClick.bind(this)
@@ -100,53 +111,35 @@ class Gemerate extends Component {
     this.parseFile = this.parseFile.bind(this)
   }
 
-  
+
 
   async componentDidMount() {
     // TONEJS
     if (
-      !(
-        window.File &&
-        window.FileReader &&
-        window.FileList &&
-        window.Blob
-      )
+        !(
+            window.File &&
+            window.FileReader &&
+            window.FileList &&
+            window.Blob
+        )
     ) {
       document.querySelector("#FileDrop #Text").textContent =
-        "Reading files not supported by this browser";
+          "Reading files not supported by this browser";
     } else {
       const fileDrop = document.querySelector("#FileDrop");
-
     }
-    
-  }
-  
-  // Snackbar handlers
-  handleClick = () => {
-      this.setState({openSnackbar: true});
-  };
-  handleClose = () => {
-      this.setState({openSnackbar: false});
-  };
 
-  // onChange handlers
-  memLengthHandler(e, value) {
-    this.setState({memoryLength: value})
-  }
-  numFilesHandler(e, value) {
-    this.setState({numberOfFiles: value})
-  }
-  temperatureHandler(e, value) {
-    this.setState({temperature: value})
   }
 
+
+  // BACKEND
 
   // Generate with TransformerGAN
   async generate() {
     //aconsole.log("to config -> ", this.state.memoryLength, this.state.numberOfFiles)
 
     // TODO -> clean MIDI data arrays, etc
-    
+
     // MAIN!!!!!!! do not delete!!!
     /*
 
@@ -210,6 +203,9 @@ class Gemerate extends Component {
     // DUMMY WORKFLOW FOR UI DEVELOPMENT!
     this.setState({generationWaitingForResponse: true, success: false, downloadDisabled: true})
 
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    await delay(5000);
+
     this.setState({generationWaitingForResponse: false})
     console.log("GENERATION RESPONSE!");
 
@@ -222,7 +218,7 @@ class Gemerate extends Component {
 
   async downloadAll() {
     console.log("Download ... ")
-    
+
     fetch('/get-files', {
       method: 'GET',
       headers: {
@@ -230,28 +226,28 @@ class Gemerate extends Component {
       }
     }).then((response) => {
       console.log("DOWNLOAD RESPONSE", response)
-        var a = response.body.getReader();
-        a.read().then(({ done, value }) => {
+      var a = response.body.getReader();
+      a.read().then(({ done, value }) => {
             saveAsFile(new TextDecoder("utf-8").decode(value), 'generated.zip');
           }
-        );
+      );
     });
-    
+
     function saveAsFile(text, filename) {
       // Step 1: Create the blob object with the text you received
       const type = 'application/text'; // modify or get it from response
       const blob = new Blob([text], {type});
-    
+
       // Step 2: Create Blob Object URL for that blob
       const url = URL.createObjectURL(blob);
-    
+
       // Step 3: Trigger downloading the object using that URL
       const a = document.createElement('a');
       a.href = url;
       a.download = filename;
       a.click(); // triggering it manually
     }
-    
+
   }
 
   // MIDI
@@ -304,8 +300,8 @@ class Gemerate extends Component {
     reader.onload = function (e) {
       midi = new Midi(e.target.result);
       document
-        .querySelector("tone-play-toggle")
-        .removeAttribute("disabled");
+          .querySelector("tone-play-toggle")
+          .removeAttribute("disabled");
       this.setCurrentMIDI(midi);
     }.bind(this);
     reader.readAsArrayBuffer(file);
@@ -356,135 +352,169 @@ class Gemerate extends Component {
       }
 
     }
-
   }
 
-  
+
+
+
+
+
+
+
+
+
+  // UI
+
+  // Snackbar handlers
+  handleClick = () => {
+    this.setState({openSnackbar: true});
+  };
+  handleClose = () => {
+    this.setState({openSnackbar: false});
+  };
+
+  // onChange handlers
+  memLengthHandler(e, value) {
+    this.setState({memoryLength: value})
+  }
+  numFilesHandler(e, value) {
+    this.setState({numberOfFiles: value})
+  }
+  temperatureHandler(e, value) {
+    this.setState({temperature: value})
+  }
+
+
 
   render() {
     return (
-      <div className="App">
+        <div className="App">
 
-        <Typography style={{textAlign: "center", paddingTop:"15px"}} variant="h1" component="h2">
-          ChordGAN
-        </Typography>;
-        
-        <header className="App-header">
-        {/*<h1 className="OldApp-title">ChordGAN</h1>*/}
-          <h4>Generative Adversarial Neural Networks for Random and Complex Chord Progression Generation</h4>
-        </header>
-        
-        <Container maxWidth="sm">
+          <Typography style={{textAlign: "center", paddingTop:"15px"}} variant="h1" component="h2">
+            ChordGAN
+          </Typography>;
 
-          <div className='upper-panel'>
-            <Stack direction="column" spacing={2} divider={<Divider orientation="vertical" flexItem />} sx={upperPanelStyling} >
-              <FormLabel id="slider1Label" sx={formLabelStyling}> Memory Length </FormLabel>
-              <Slider 
-                sx={sliderMemoryLengthStyling}
-                aria-label="MemoryLength" 
-                valueLabelDisplay="auto" 
-                min={100} max={2070} step={10} 
-                value={this.state.memoryLength} 
-                onChange={this.memLengthHandler}
-              />
-              
-              <FormLabel id="slider2Label" sx={sliderLabelNumberOfFilesStyling}> Number of Files </FormLabel>
-              <Slider 
-                sx={sliderMemoryLengthStyling} 
-                aria-label="NumberOfFiles" 
-                valueLabelDisplay="auto" 
-                min={1} max={5} step={1} 
-                value={this.state.numberOfFiles} 
-                onChange={this.numFilesHandler}
-              />
+          <header className="App-header">
+            {/*<h1 className="OldApp-title">ChordGAN</h1>*/}
+            <h4>Generative Adversarial Neural Networks for Random and Complex Chord Progression Generation</h4>
+          </header>
 
-              <FormLabel id="slider3Label" sx={sliderLabelNumberOfFilesStyling}> Temperature</FormLabel>
-              <Slider 
-                sx={sliderMemoryLengthStyling} 
-                aria-label="Temperature" 
-                valueLabelDisplay="auto"
-                min={0.80} max={1.20} step={0.01} 
-                value={this.state.temperature} 
-                onChange={this.temperatureHandler}
-              />
-            </Stack>
+          <Container maxWidth="sm">
 
-            <Stack direction="row" spacing={3} divider={<Divider orientation="vertical" flexItem />} sx={lowerPanelStyling} >
-              <Button variant="text" className='Generate-Button' sx={generateButtonStyling} onClick={this.generate}>Generate</Button>
-              {/* <Button href="http://localhost:5000/get-files" disabled={this.state.downloadDisabled} variant="contained" className='Download-Button' sx={generateButtonStyling} download="generated.zip">Download</Button> */}
-            </Stack>
-          </div>
+            <div className='upper-panel'>
+              <Stack direction="column" spacing={2} divider={<Divider orientation="vertical" flexItem />} className='upperPanelStyling' >
+                <FormLabel id="slider1Label"  > Memory Length </FormLabel>
+                <CustomSlider
+                    aria-label="MemoryLength"
+                    valueLabelDisplay="auto"
+                    min={100} max={2070} step={10}
+                    value={this.state.memoryLength}
+                    onChange={this.memLengthHandler}
+                />
 
-          {this.state.success ? 
+                <FormLabel id="slider2Label" > Number of Files </FormLabel>
+                <CustomSlider
+                    aria-label="NumberOfFiles"
+                    valueLabelDisplay="auto"
+                    min={1} max={5} step={1}
+                    value={this.state.numberOfFiles}
+                    onChange={this.numFilesHandler}
+                />
 
-            <>
-              <div className='lower-panel'>
-                <Stack direction="column" spacing={2} divider={<Divider orientation="vertical" flexItem />} sx={lowerMIDIPanel} >
-                  <FormLabel id="MIDISelectLabel" sx={MIDISelectLabel}> Select a file and press play to hear an audio preview </FormLabel>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">MIDI</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      label="Age"
-                      onChange={this.selectedMIDIHandler}
-                    >
-                      
-                      {this.state.filesArray.map(item => {
-                        return <MenuItem value={item}>{item}</MenuItem>;
-                      })}
+                <FormLabel id="slider3Label" > Temperature </FormLabel>
+                <CustomSlider
+                    aria-label="Temperature"
+                    valueLabelDisplay="auto"
+                    min={0.80} max={1.20} step={0.01}
+                    value={this.state.temperature}
+                    onChange={this.temperatureHandler}
+                />
 
-                    </Select>
-                  </FormControl>
-                </Stack>
-                
-                <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />} sx={lowerPanelStyling} >
+              </Stack>
 
-                  <div>
-                    <tone-content>
-                      <tone-play-toggle
-                        ref={this.player}
-                        disabled={this.state.isPlaying}
-                        onClick={this.playSelectedFile}
-                      >
-                      </tone-play-toggle>
-                    </tone-content>
+              <Stack direction="row" spacing={3} divider={<Divider orientation="vertical" flexItem />} className='lowerPanelStyling' >
+                <CustomButton variant="text"  onClick={this.generate}>Generate</CustomButton>
+                {/* <Button href="http://localhost:5000/get-files" disabled={this.state.downloadDisabled} variant="contained" className='Download-Button' sx={generateButtonStyling} download="generated.zip">Download</Button> */}
+              </Stack>
+            </div>
+
+            {this.state.success ?
+
+                <>
+                  <div className='lower-panel'>
+                    <Stack direction="column" spacing={2} divider={<Divider orientation="vertical" flexItem />} className='lowerMIDIPanel' >
+                      <CustomFormLabel id="MIDISelectLabel" sx={{marginTop:-3}}> Select a file and press play to hear an audio preview </CustomFormLabel>
+                      <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">MIDI</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                            onChange={this.selectedMIDIHandler}
+                        >
+
+                          {this.state.filesArray.map(item => {
+                            return <MenuItem value={item}>{item}</MenuItem>;
+                          })}
+
+                        </Select>
+                      </FormControl>
+                    </Stack>
+
+                    <Stack direction="row" spacing={1} className='lowerPanelStyling' >
+
+                      <div className='customPlayer'>
+                        <tone-content>
+                          <tone-play-toggle
+                              ref={this.player}
+                              disabled={this.state.isPlaying}
+                              onClick={this.playSelectedFile}
+                          >
+                          </tone-play-toggle>
+                        </tone-content>
+                      </div>
+                      <div className='downloadButtonStyling'>
+                        <CustomButton href='http://localhost:5000/getFile/' disabled={this.state.selectedMIDI && false} variant="text"  download={this.state.selectedMIDI}>Download Selected</CustomButton>
+                      </div>
+                      <div className='downloadButtonStyling'>
+                        <CustomButton href="http://localhost:5000/get-files" disabled={this.state.downloadDisabled} variant="text" className='Download-Button'  download="generated.zip">Download All</CustomButton>
+                      </div>
+                    </Stack>
+
+                    <br></br>
+                    <br></br>
+                    <p>{JSON.stringify(this.state)}</p>
                   </div>
-                  <Button href='http://localhost:5000/getFile/' disabled={this.state.selectedMIDI && false} variant="text" className='Download-Button' sx={downloadButtonStyling} download={this.state.selectedMIDI}>Download Selected</Button>
-                  <Button href="http://localhost:5000/get-files" disabled={this.state.downloadDisabled} variant="text" className='Download-Button' sx={downloadButtonStyling} download="generated.zip">Download All</Button>
-                </Stack>
-                
-                <br></br>
-                <br></br>
-                <p>{JSON.stringify(this.state)}</p>
-              </div>
-            </>
-            :
-            <>
-              <div className='lower-panel'>
-                {this.state.generationWaitingForResponse &&
-                  <CircularProgress size={90} color="inherit" />
-                }
-              </div>
-              <Snackbar
-                anchorOrigin={{'vertical':this.state.verticalSnackbar, 'horizontal':this.state.horizontalSnackbar}}
-                open={this.state.openSnackbar}
-                onClose={this.handleClose}
-              >
-                  <Alert onClose={this.handleClose} severity="error" sx={{ width: '100%' }}>
-                    An error occured when trying to generate data
-                  </Alert>
-              </Snackbar>
+                </>
+                :
+                <>
+                  <div className='lower-panel' >
+                    <div style={{textAlign: "center"}}>
+                      {this.state.generationWaitingForResponse &&
+                      <CircularProgress className='border' thickness={18} size={90} color="inherit" />
+                      }
+                    </div>
 
-            </>
-          }
+                  </div>
+                  <Snackbar
+                      anchorOrigin={{'vertical':this.state.verticalSnackbar, 'horizontal':this.state.horizontalSnackbar}}
+                      open={this.state.openSnackbar}
+                      onClose={this.handleClose}
+                  >
+                    <Alert onClose={this.handleClose} severity="error" sx={{ width: '100%' }}>
+                      An error occured when trying to generate data
+                    </Alert>
+                  </Snackbar>
 
-        </Container>
-        
+                </>
+            }
 
-      </div>
+          </Container>
+
+
+        </div>
     );
   }
 }
 
-export default Gemerate;
+export default Generate;
