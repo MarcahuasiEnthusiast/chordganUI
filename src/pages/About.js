e7057432a274b43de50125f9107a0c75977e6b81
peruviantorch <robertovegab777@gmail.com>
2022-09-23 23:46:18 -0500

    EA

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return <div className="Route-Text">
+        <h2>
+            Generative Adversarial Neural Networks for Random and Complex Chord Progression Generation
+        </h2>
+    </div>
+};
+
+export default About;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return <div className="Route-Text">
+        <h2>
+            Generative Adversarial Neural Networks for Random and Complex Chord Progression Generation
+        </h2>
+    </div>
+};
+
+export default Home;
